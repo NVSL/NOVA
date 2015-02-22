d98347e19af3c29d18990d8045d56fdefda4403e
Andiry Xu <jix024@cs.ucsd.edu>
2015-02-21 21:18:04 -0800

    8G init

--- setup-pmfs.sh
+++ setup-pmfs.sh
@@ -6,7 +6,7 @@ insmod pmfs.ko
 
 sleep 1
 
-mount -t pmfs -o physaddr=0x100000000,init=2G none /mnt/ramdisk
+mount -t pmfs -o physaddr=0x100000000,init=8G none /mnt/ramdisk
 
 #cp test1 /mnt/ramdisk/
 #dd if=/dev/zero of=/mnt/ramdisk/test1 bs=1M count=1024 oflag=direct
