cdc7c888e67ee058234a4705818b9d01e2a7369b
Andiry Xu <jix024@cs.ucsd.edu>
2015-03-04 21:15:28 -0800

    Remove 0001 patch

--- xip.c
+++ xip.c
@@ -661,6 +661,7 @@ static int __pmfs_xip_file_fault(struct vm_area_struct *vma,
 			" vm_end(0x%lx), pgoff(0x%lx), VA(%lx)\n",
 			__func__, __LINE__, vma->vm_start, vma->vm_end,
 			vmf->pgoff, (unsigned long)vmf->virtual_address);
+		dump_stack();
 		return VM_FAULT_SIGBUS;
 	}
 
