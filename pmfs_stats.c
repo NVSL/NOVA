36d81a3d5b304a59429fad960e3ba25c7b942db5
Andiry Xu <jix024@cs.ucsd.edu>
2015-03-29 10:14:34 -0700

    Page cache write break counting

--- pmfs_stats.c
+++ pmfs_stats.c
@@ -52,18 +52,22 @@ void pmfs_print_blocknode_list(struct super_block *sb)
 	}
 	mutex_unlock(&sbi->s_lock);
 	printk("All: %lu nodes\n", count);
-	printk("alloc %llu, alloc steps %lu, average %llu\n",
+	printk("Alloc %llu, alloc steps %lu, average %llu\n",
 		Countstats[new_data_blocks_t], alloc_steps,
 		Countstats[new_data_blocks_t] ?
 			alloc_steps / Countstats[new_data_blocks_t] : 0);
-	printk("free %llu, free steps %lu, average %llu\n",
+	printk("Free %llu, free steps %lu, average %llu\n",
 		Countstats[free_data_t], free_steps,
 		Countstats[free_data_t] ?
 			free_steps / Countstats[free_data_t] : 0);
-	printk("write %llu, write breaks %lu, average %llu\n",
+	printk("COW write %llu, write breaks %lu, average %llu\n",
 		Countstats[cow_write_t], write_breaks,
 		Countstats[cow_write_t] ?
 			write_breaks / Countstats[cow_write_t] : 0);
+	printk("Page cache write %llu, write breaks %lu, average %llu\n",
+		Countstats[page_cache_write_t], write_breaks,
+		Countstats[page_cache_write_t] ?
+			write_breaks / Countstats[page_cache_write_t] : 0);
 }
 
 void pmfs_print_timing_stats(struct super_block *sb)
