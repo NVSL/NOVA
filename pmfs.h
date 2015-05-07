8ccf3e47e438748ed351500a6ddb2e9b281ceab2
Andiry Xu <jix024@cs.ucsd.edu>
2015-05-07 13:22:52 -0700

    Remove comma in macro define

--- pmfs.h
+++ pmfs.h
@@ -365,13 +365,13 @@ struct pmfs_dir_node *pmfs_alloc_dirnode(struct super_block *sb);
 void pmfs_free_dirnode(struct super_block *sb, struct pmfs_dir_node *node);
 
 #define _mm_clflush(addr)\
-	asm volatile("clflush %0" : "+m" (*(volatile char *)(addr)));
+	asm volatile("clflush %0" : "+m" (*(volatile char *)(addr)))
 #define _mm_clflushopt(addr)\
-	asm volatile(".byte 0x66; clflush %0" : "+m" (*(volatile char *)(addr)));
+	asm volatile(".byte 0x66; clflush %0" : "+m" (*(volatile char *)(addr)))
 #define _mm_clwb(addr)\
-	asm volatile(".byte 0x66; xsaveopt %0" : "+m" (*(volatile char *)(addr)));
+	asm volatile(".byte 0x66; xsaveopt %0" : "+m" (*(volatile char *)(addr)))
 #define _mm_pcommit()\
-	asm volatile(".byte 0x66, 0x0f, 0xae, 0xf8");
+	asm volatile(".byte 0x66, 0x0f, 0xae, 0xf8")
 
 /* Provides ordering from all previous clflush too */
 static inline void PERSISTENT_MARK(void)
