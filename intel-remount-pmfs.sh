b3384726f368101bc01d6f9d50cb93d8cdab0d8e
Andiry Xu <jix024@cs.ucsd.edu>
2015-05-16 15:30:53 -0700

    Setup scripts fix

--- intel-remount-pmfs.sh
+++ intel-remount-pmfs.sh
@@ -6,8 +6,7 @@ insmod pmfs.ko measure_timing=0 support_clwb=1
 
 sleep 1
 
-#mount -t pmfs -o physaddr=0x100000000 none /mnt/ramdisk
-mount -t pmfs -o physaddr=0x10000000000 none /mnt/ramdisk
+mount -t pmfs -o physaddr=0x10000000000 CoolFS /mnt/ramdisk
 
 #cp test1 /mnt/ramdisk/
 #dd if=/dev/zero of=/mnt/ramdisk/test1 bs=1M count=1024 oflag=direct
--- remount-pmfs.sh
+++ remount-pmfs.sh
@@ -6,7 +6,7 @@ insmod pmfs.ko measure_timing=0
 
 sleep 1
 
-mount -t pmfs -o physaddr=0x100000000 none /mnt/ramdisk
+mount -t pmfs -o physaddr=0x100000000 CoolFS /mnt/ramdisk
 
 #cp test1 /mnt/ramdisk/
 #dd if=/dev/zero of=/mnt/ramdisk/test1 bs=1M count=1024 oflag=direct
