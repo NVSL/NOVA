70f4aad9eaa186c4eaabfa5e082efaa754004f31
Andiry Xu <jix024@cs.ucsd.edu>
2015-07-03 22:01:01 -0700

    Remove unnecessary declarations from xip.h

--- xip.h
+++ xip.h
@@ -10,19 +10,8 @@
  * warranty of any kind, whether express or implied.
  */
 
-int pmfs_get_xip_mem(struct address_space *, pgoff_t, int, void **,
-		      unsigned long *);
 ssize_t pmfs_xip_file_read(struct file *filp, char __user *buf, size_t len,
 			    loff_t *ppos);
 ssize_t pmfs_xip_file_write(struct file *filp, const char __user *buf,
 		size_t len, loff_t *ppos);
 int pmfs_xip_file_mmap(struct file *file, struct vm_area_struct *vma);
-
-static inline int pmfs_use_xip(struct super_block *sb)
-{
-	struct pmfs_sb_info *sbi = PMFS_SB(sb);
-
-	return sbi->s_mount_opt & PMFS_MOUNT_XIP;
-}
-
-#define mapping_is_xip(map) (map->a_ops->get_xip_mem)
