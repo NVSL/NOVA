68c07dc0282bfef7c33dcee0887868e6312420d8
Andiry Xu <jix024@cs.ucsd.edu>
2015-07-19 15:41:23 -0700

    Use 64G on PMEP paltform

--- intel-setup-pmfs.sh
+++ intel-setup-pmfs.sh
@@ -7,7 +7,7 @@ insmod pmfs.ko measure_timing=0 support_clwb=0
 
 sleep 1
 
-mount -t pmfs -o physaddr=0x10000000000,init=32G CoolFS /mnt/ramdisk
+mount -t pmfs -o physaddr=0x10000000000,init=64G CoolFS /mnt/ramdisk
 
 #cp test1 /mnt/ramdisk/
 #dd if=/dev/zero of=/mnt/ramdisk/test1 bs=1M count=1024 oflag=direct
