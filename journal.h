8019117961a4ed76d01b61034e80bb5af1cc2210
Andiry Xu <jix024@cs.ucsd.edu>
2015-07-26 08:51:50 -0700

    Remove journal completely

--- journal.h
+++ journal.h
@@ -19,75 +19,9 @@
 #define __PMFS_JOURNAL_H__
 #include <linux/slab.h>
 
-/* default pmfs journal size 4MB */
-#define PMFS_DEFAULT_JOURNAL_SIZE  (4 << 20)
-/* minimum pmfs journal size 64KB */
-#define PMFS_MINIMUM_JOURNAL_SIZE  (1 << 16)
-
 #define CACHELINE_SIZE  (64)
-#define CLINE_SHIFT		(6)
 #define CACHELINE_MASK  (~(CACHELINE_SIZE - 1))
 #define CACHELINE_ALIGN(addr) (((addr)+CACHELINE_SIZE-1) & CACHELINE_MASK)
-
-#define LOGENTRY_SIZE  CACHELINE_SIZE
-#define LESIZE_SHIFT   CLINE_SHIFT
-
-/* FIXME: Limit inode size to 96 bytes */
-#define MAX_INODE_LENTRIES (3)
-#define MAX_SB_LENTRIES (2)
-/* 1 le for dir entry and 1 le for potentially allocating a new dir block */
-#define MAX_DIRENTRY_LENTRIES   (2)
-/* 2 le for adding or removing the inode from truncate list. used to log
- * potential changes to inode table's i_next_truncate and i_sum */
-#define MAX_TRUNCATE_LENTRIES (2)
-#define MAX_DATA_PER_LENTRY  48
-/* blocksize * max_btree_height */
-#define MAX_METABLOCK_LENTRIES \
-	((PMFS_DEF_BLOCK_SIZE_4K * 3)/MAX_DATA_PER_LENTRY)
-
-#define MAX_PTRS_PER_LENTRY (MAX_DATA_PER_LENTRY / sizeof(u64))
-
-#define TRANS_RUNNING    1
-#define TRANS_COMMITTED  2
-#define TRANS_ABORTED    3
-
-#define LE_DATA        0
-#define LE_START       1
-#define LE_COMMIT      2
-#define LE_ABORT       4
-
-#define MAX_GEN_ID  ((uint16_t)-1)
-
-/* persistent data structure to describe a single log-entry */
-/* every log entry is max CACHELINE_SIZE bytes in size */
-typedef struct {
-	__le64   addr_offset;
-	__le32   transaction_id;
-	__le16   gen_id;
-	u8       type;  /* normal, commit, or abort */
-	u8       size;
-	char     data[48];
-} pmfs_logentry_t;
-
-
-#if 0
-extern pmfs_transaction_t *pmfs_alloc_transaction(void);
-extern void pmfs_free_transaction(pmfs_transaction_t *trans);
-
-extern int pmfs_journal_soft_init(struct super_block *sb);
-extern int pmfs_journal_hard_init(struct super_block *sb,
-		uint64_t base, uint32_t size);
-extern int pmfs_journal_uninit(struct super_block *sb);
-extern pmfs_transaction_t *pmfs_new_transaction(struct super_block *sb,
-		int nclines);
-extern int pmfs_add_logentry(struct super_block *sb,
-		pmfs_transaction_t *trans, void *addr, uint16_t size, u8 type);
-extern int pmfs_commit_transaction(struct super_block *sb,
-		pmfs_transaction_t *trans);
-extern int pmfs_abort_transaction(struct super_block *sb,
-			pmfs_transaction_t *trans);
-extern int pmfs_recover_journal(struct super_block *sb);
-#endif
 
 /* Lite journal */
 struct pmfs_lite_journal_entry {
