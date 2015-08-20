dfcca0d9adb76ffd0737aca9a5fa511c2c7e03fa
Andiry Xu <jix024@cs.ucsd.edu>
2015-08-20 15:53:00 -0700

    Add info in remount scripts

--- intel-remount-pmfs.sh
+++ intel-remount-pmfs.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+echo "Unmount existing partition..."
 umount /mnt/ramdisk
 rmmod pmfs
 insmod pmfs.ko measure_timing=0
 
+echo "Unmount done."
 sleep 1
+
+echo "Mounting..."
 
 mount -t pmfs -o physaddr=0x10000000000 CoolFS /mnt/ramdisk
 
--- remount-pmfs.sh
+++ remount-pmfs.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+echo "Unmount existing partition..."
 umount /mnt/ramdisk
 rmmod pmfs
 insmod pmfs.ko measure_timing=0
 
+echo "Unmount done."
 sleep 1
 
+echo "Mounting..."
 mount -t pmfs -o physaddr=0x400000000 CoolFS /mnt/ramdisk
 
 #cp test1 /mnt/ramdisk/
