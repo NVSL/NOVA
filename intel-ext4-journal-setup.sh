f6725b26828096990de26eb5dba2a0f817f18877
Andiry Xu <jix024@cs.ucsd.edu>
2015-08-21 00:42:05 -0700

    Add Intel ext4 setup scripts

---
+++ intel-ext4-journal-setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+umount /mnt/ramdisk
+rmmod pmem
+rmmod pmfs
+insmod pmem.ko pmem_start_gb=1024 pmem_size_gb=64 measure_timing=0
+
+sleep 1
+
+mkfs.ext4 /dev/pmem0
+mount -o data=journal /dev/pmem0 /mnt/ramdisk
+
+#cp test1 /mnt/ramdisk/
+#dd if=/dev/zero of=/mnt/ramdisk/test1 bs=1M count=1024 oflag=direct
